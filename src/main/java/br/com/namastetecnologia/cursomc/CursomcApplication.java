e0efddcdd4106e867af6a20cc496224bc6dccead
Rodrigo Almeida <rodrigo@namastetecnologia.com.br>
2018-08-21 09:58:15 -0300

    Banco de dados H2 e criação automatica da base de dados

--- src/main/java/br/com/namastetecnologia/cursomc/CursomcApplication.java
+++ src/main/java/br/com/namastetecnologia/cursomc/CursomcApplication.java
@@ -1,9 +1,13 @@
 package br.com.namastetecnologia.cursomc;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EntityScan("br.com.namastetecnologia.cursomc.domain")
 public class CursomcApplication {
 
 	public static void main(String[] args) {
