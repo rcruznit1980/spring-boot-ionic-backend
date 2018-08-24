b0afe6f5ddc628de3005d45d4e38e3f6d6178600
Rodrigo Almeida <rodrigo@namastetecnologia.com.br>
2018-08-24 14:33:15 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/br/com/namastetecnologia/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package br.com.namastetecnologia.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.namastetecnologia.cursomc.domain.Cliente;
+import br.com.namastetecnologia.cursomc.repositories.ClienteRepository;
+import br.com.namastetecnologia.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {  
+		Optional<Cliente> obj = repo.findById(id);  
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id + ", Tipo: " + Cliente.class.getName())); 
+		
+	} 
+	
+}
