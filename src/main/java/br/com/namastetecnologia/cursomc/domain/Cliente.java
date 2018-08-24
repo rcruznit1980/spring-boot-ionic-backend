3c963e3a545e7cf1479469bbb0d70da526dd8119
Rodrigo Almeida <rodrigo@namastetecnologia.com.br>
2018-08-24 17:43:59 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/br/com/namastetecnologia/cursomc/domain/Cliente.java
+++ src/main/java/br/com/namastetecnologia/cursomc/domain/Cliente.java
@@ -14,8 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import br.com.namastetecnologia.cursomc.enums.TipoCliente;
 
@@ -25,23 +24,22 @@ public class Cliente implements Serializable {
 	private static final long serialVersionUID = -8991389603368101192L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String email;
 	private String cpfCnpj;
 	private Integer tipo;
-	
-	@JsonManagedReference
-	@OneToMany(mappedBy="cliente")
+
+	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
-	
+
 	@ElementCollection
-	@CollectionTable(name="TELEFONE")
+	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
-	
-	@JsonBackReference
-	@OneToMany(mappedBy="cliente")
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 
 	public Cliente() {
@@ -113,7 +111,7 @@ public class Cliente implements Serializable {
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
-	
+
 	public List<Pedido> getPedidos() {
 		return pedidos;
 	}
@@ -147,8 +145,4 @@ public class Cliente implements Serializable {
 		return true;
 	}
 
-	
-	
-	
-	
 }
--- src/main/java/br/com/namastetecnologia/cursomc/domain/Produto.java
+++ src/main/java/br/com/namastetecnologia/cursomc/domain/Produto.java
@@ -15,9 +15,6 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
-import org.hibernate.annotations.ManyToAny;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
@@ -31,7 +28,7 @@ public class Produto implements Serializable {
 	private String nome;
 	private Double preco;
 	
-	@JsonBackReference //tratando referencia cíclica
+	@JsonIgnore
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA", 	
 		joinColumns = @JoinColumn(name="produto_id"),
