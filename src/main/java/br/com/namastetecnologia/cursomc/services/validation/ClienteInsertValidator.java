45683316d37f008fa65b36b4b247a20b9437ab1f
Rodrigo Almeida <rodrigo@namastetecnologia.com.br>
2018-08-27 13:20:19 -0300

    Validacao customizada de CPF ou CNPJ na insercao de Cliente

---
+++ src/main/java/br/com/namastetecnologia/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,41 @@
+package br.com.namastetecnologia.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import br.com.namastetecnologia.cursomc.dto.ClienteNewDTO;
+import br.com.namastetecnologia.cursomc.enums.TipoCliente;
+import br.com.namastetecnologia.cursomc.resources.exceptions.FieldMessage;
+import br.com.namastetecnologia.cursomc.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+		
+		// inclua os testes aqui, inserindo erros na lista
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfCnpj()) ) {
+			list.add(new FieldMessage("cpfCnpj", "CPF inválido"));
+		}
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfCnpj()) ) {
+			list.add(new FieldMessage("cpfCnpj", "CNPJ inválido"));
+		}
+		
+		
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
