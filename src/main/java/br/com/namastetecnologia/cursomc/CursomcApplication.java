d2214a20510ddcaace51f167b072ac93451eb9de
Rodrigo Almeida <rodrigo@namastetecnologia.com.br>
2018-09-03 16:23:15 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/br/com/namastetecnologia/cursomc/CursomcApplication.java
+++ src/main/java/br/com/namastetecnologia/cursomc/CursomcApplication.java
@@ -1,22 +1,28 @@
 package br.com.namastetecnologia.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+import br.com.namastetecnologia.cursomc.services.S3Service;
+
 @SpringBootApplication
 @EnableAutoConfiguration
 @EntityScan("br.com.namastetecnologia.cursomc.domain")
 public class CursomcApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		s3Service.uploadFile("C:\\Temp\\imagens\\darkSideOfTheMoon.jpg");
 	}
 }
